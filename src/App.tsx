3794369dd05c0a563f6785b15ec285da91992144
dhanesh <dhaneshpk359@gmail.com>
2023-12-21 22:44:31 +0530

    first message

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import "./globals.css"
+import SIgnINForm from './_auth/forms/SIgnINForm'
+import { Home } from './_root/pages'
+import AuthLayout from './_auth/AuthLayout'
+import SIgnupForm from './_auth/forms/SIgnupForm'
+import RootLayout from './_root/RootLayout'
 
 
 function App() {
@@ -8,11 +13,17 @@ function App() {
     <main className='flex h-screen'>
         <Routes>
             {/*public routes*/}
-            <Route path='/Sign-in' element={<SignInForm/>}/>
-            <Route path='/Login' element={<Login/>}/>
+            <Route element={<AuthLayout/>}>
+              <Route path='/Sign-in' element={<SIgnINForm/>}/>
+              <Route path='/Sign-up' element={<SIgnupForm/>}/>
+            </Route>
+            
 
             {/* private routes */}
-            <Route path='/Home' element={<Home/>}/>
+            <Route element={<RootLayout/>}>
+                <Route index element={<Home/>}/>
+
+            </Route>
         </Routes>
     </main>
   )
